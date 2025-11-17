f06019a6996263869a0f8421cbf87f148dcc9633
YassarM <yassarmuwaffaq93@gmail.com>
2025-11-18 02:57:40 +0700

    Landing Page

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import LandingPage from './component/LandingPage.jsx'
+
+function App() {
+
+  return (
+    <>
+      <LandingPage></LandingPage>
+    </>
+  )
+}
+
+export default App
---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        dm: ["DM Sans", "sans-serif"],
+      },
+    },
+  },
+}
